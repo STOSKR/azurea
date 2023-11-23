7a7284b0d66a52465eb71f44f0fd4006496bd6b9
Shiyi Cheng <SCHENG1@upv.edu.es>
2023-11-23 23:37:05 +0100

    aaasd

--- src/App.js
+++ src/App.js
@@ -1,8 +1,33 @@
-import React from 'react';
+import React, { useState } from 'react';
 
 function App() {
-  const value = 'World';
-  return <div>Hello </div>;
+  const [imageUrl, setImageUrl] = useState('');
+  const [imageRequest, setImageRequest] = useState('');
+
+  const handleImageAnalysis = () => {
+    // Perform image analysis logic here
+    console.log('Image analysis triggered');
+  };
+
+  const handleImageGeneration = () => {
+    // Perform image generation logic here
+    console.log('Image generation triggered');
+  };
+
+  return (
+    <div>
+      <h1>Title</h1>
+      <h1>Title</h1>
+      <input
+        type="text"
+        value={imageUrl}
+        onChange={(e) => setImageUrl(e.target.value)}
+        placeholder="Enter image URL or request"
+      />
+      <button onClick={handleImageAnalysis}>Analyze Image</button>
+      <button onClick={handleImageGeneration}>Generate Image</button>
+    </div>
+  );
 }
 
 export default App;
